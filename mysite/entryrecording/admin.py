dc692d3046889ebdc9ff2220a86eb71e39d6ad1b
TubagusHamzahAchyari <121467099+wproject505@users.noreply.github.com>
2023-05-14 23:30:47 +0700

    second commit

--- mysite/entryrecording/admin.py
+++ mysite/entryrecording/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, CustomerData, Purchase
+
+class CustomerDataAdmin(admin.ModelAdmin):
+    list_display = ['customer_number', 'name', 'phone', 'email', 'address', 'postal_zip', 'region', 'country']
+    list_filter = ['region', 'country']
+    search_fields = ['name', 'email', 'region', 'address']
+    list_editable = ['phone']
+    list_display_links = ['customer_number']
+    def __str__(self):
+        return self.name
+
 
 admin.site.register(Customer)
+admin.site.register(CustomerData, CustomerDataAdmin)
+admin.site.register(Purchase)
 
 
